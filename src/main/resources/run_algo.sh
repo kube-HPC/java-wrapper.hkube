c44ef62bc35e89d0d7591cf1e73d8f6749238293
golanha <golanha@rnd-hub.com>
2020-11-16 12:41:31 +0200

    1.3.02

--- src/main/resources/run_algo.sh
+++ src/main/resources/run_algo.sh
@@ -1,3 +1,3 @@
 export WORKER_SOCKET_PORT=5555
 export ALGORITHM_ENTRY_POINT=Algorithm
-java -jar ../../../wrapper/target/wrapper-wide-1.3.02.jar ../../../encapsulated-algorithm.jar
+java -jar ../../../wrapper/target/wrapper-wide-1.3.03.jar ../../../encapsulated-algorithm.jar
