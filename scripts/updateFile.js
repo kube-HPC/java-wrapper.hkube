8171d90fbe87ff1a9088abd2787fba138f9062a6
golanha <golanha@rnd-hub.com>
2021-08-17 09:06:40 +0300

    generalize update builder script

--- scripts/updateFile.js
+++ scripts/updateFile.js
@@ -4,29 +4,29 @@ const { Octokit } = require("@octokit/rest");
 
 const ownerRepo = {
     owner: 'kube-hpc',
-    repo: 'hkube'
+    repo: process.env.repository
 };
 
 const main = async () => {
-    const version = process.env.revision+process.env.suffix;
-    const file = 'core/algorithm-builder/environments/java/wrapper/version.txt';
+    const value = process.env.fileContent;
+    const file = process.env.updatedFilePath;
     const octokit = new Octokit({ auth: process.env.GH_TOKEN });
     const packageJsonContentResponse = await octokit.repos.getContent({
         ...ownerRepo,
         path: file
     });
-    const versionFile = Buffer.from(packageJsonContentResponse.data.content, 'base64').toString('utf-8');
+    const valueFile = Buffer.from(packageJsonContentResponse.data.content, 'base64').toString('utf-8');
 
-    const versionFileSha = packageJsonContentResponse.data.sha;
+    const valueFileSha = packageJsonContentResponse.data.sha;
     // update package json
-    const newContent = Buffer.from(version).toString('base64');
+    const newContent = Buffer.from(value).toString('base64');
 
     const masterShaResponse = await octokit.repos.listCommits({
         ...ownerRepo,
         per_page: 1
     });
     const masterSha = masterShaResponse.data[0].sha;
-    const branchName = `update_java_wraper_to_${version.replace('.', '_')}`;
+    const branchName = `update_java_wraper_to_${value.replace('.', '_')}`;
     await octokit.git.createRef({
         ...ownerRepo,
         ref: `refs/heads/${branchName}`,
@@ -35,15 +35,15 @@ const main = async () => {
     await octokit.repos.createOrUpdateFileContents({
         ...ownerRepo,
         path: file,
-        message: `update java wrapper to ${version}`,
+        message: `update {file} to ${value}`,
         branch: branchName,
-        sha: versionFileSha,
+        sha: valueFileSha,
         content: newContent
     });
 
     await octokit.pulls.create({
         ...ownerRepo,
-        title: `update java wrapper to ${version}`,
+        title: `update {file} to ${value}`,
         head: branchName,
         base: 'master'
     });
